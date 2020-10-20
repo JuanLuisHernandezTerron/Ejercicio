9dcd1fb7e03fc0fda5efaf7b61df79a0491e7239
daw1 <alum.jluishernandezt@iesalixar.org>
2020-10-20 20:24:05 +0200

    Saludo en colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,9 @@
 class HolaMundo {
     public static void main( String[] args) {
-	String naranja = "\003[33m";
+	String naranja = "\033[33m";
 	String azul = "\033[34m";
 
-        System.out.print( "Hola Mundo!" );
-	System.out.println("HolaMundo2!");
+        System.out.println(naranja + "Hola Mundo!" );
+	System.out.println(azul + "HolaMundo2!");
     }
 }
